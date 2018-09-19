110bb9eeaf1c3160a797fa83b77d69de6f1a5a0d
Alexander <alexander09cruz@hotmail.com>
2018-09-18 20:24:43 -0600

    Creation of the hello world php and html docs

---
+++ helloW.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dynamic HW</title>
+</head>
+<body background="http://www.meteoweb.eu/wp-content/uploads/2012/01/betelgeuse-orione.jpg">
+
+	<h1 style="color: #ffffff"> <center><?php echo '<p>Hello World</p>'; ?></center></h1>
+</body>
+</html>
+
